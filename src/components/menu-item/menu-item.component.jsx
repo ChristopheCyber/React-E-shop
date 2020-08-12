19c3cd244eebd72e58f448bafd94e476b7ac97a3
Christophe <christophalpha@yahoo.com>
2020-08-12 20:09:26 +0200

    S66; continues dividing in pages & reusable components + SCSS

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './menu-item.styles.scss';
+
+const MenuItem = () => {
+    return (
+        <div className="menu-item">
+            <div className="content">
+                <h1 className="title">ITEM 1 Hats</h1>
+                <span className="subtitle">Shop Now</span>
+            </div>
+        </div>    
+        );
+}
+
+export default MenuItem;
+
