47a6f5f08bd83f1be12b578c0ed0fcc84ea42ed8
Christophe <christophalpha@yahoo.com>
2020-08-19 18:27:05 +0200

    S67; CSS SCSS styling 2

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { HomePage } from './pages/homepage/homepage.component.jsx';
 function App() {
   return (
     <div className="App bigLow">
-      <h1> E-shop S66 Fetch Require </h1>
+      <h1> E-shop S67 Styling </h1>
       <HomePage />
     </div>
   );
