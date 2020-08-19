7688a77582937ab69e3a595631b24d6289e591d0
Christophe <christophalpha@yahoo.com>
2020-08-19 10:10:34 +0200

    S66;dynamic CSS:<tag className={class1 dolars{elt.propClass2}}>; SCSS nested .class1{&.class2{CSS}}

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,20 +3,15 @@ import './menu-item.styles.scss';
 
 const MenuItem = (item) => {
     return (
-        <div className="menu-item"
+        <div className={`menu-item ${item.ItemProp5}`}
         style={ //dynamic CSS
-
             // <img src={ require(`./img/img${prop1.cardProp2.id}.jpg`)} 
             // alt={`Pic ${prop1.cardProp2.id}`} 
             // title={`Image ${prop1.cardProp2.id}`}
-
              { backgroundImage: 
                 `url(${require(`./img/${item.ItemProp4}`)})`
-                ,height: `${item.ItemProp5}`
-                ,width: `${item.ItemProp5}`
                 ,border: `3px solid pink`
              }
-             //<img src={require(`./img/WOMEN.jpg`)}></img>
         }>
             <div className="content">
                 <h1 className="title">
