ec5593ec8cf6fbd912e68cff9a49b9af9730d2f8
Christophe <christophalpha@yahoo.com>
2020-09-25 05:00:59 +0200

    S96; better sign-in-and-up style

--- src/pages/sign-in-and-up/sign-in-and-up.component.jsx
+++ src/pages/sign-in-and-up/sign-in-and-up.component.jsx
@@ -5,17 +5,23 @@ import { Link } from "react-router-dom";
 const signInAndUpComponent = () => {
     return (
         <div className="sign-in-and-up">
-            <h4>SIGN IN:</h4>
-            <span>I already have an account</span>       
-            <span>Or I want to sign in with my Google account :</span>       
-            <Link to="./signin" className="link">
-                Sign-In
-            </Link>
-            <br />
-            <span>I need to create an account :</span>       
-            <Link to="./signup" className="link">
-                Sign-Up
-            </Link>
+            <div className="bloc">
+                <h1>Sign-In :</h1>
+                <div className="sub-bloc">
+                    <span>I already have an account</span>
+                    <span>Or I want to sign in with my Google account : </span>
+                    <Link to="./signin" className="link">
+                        Sign-In
+                    </Link>
+                </div>
+                <br />
+                <div className="sub-bloc">
+                    <span>I need to create an account :</span>
+                    <Link to="./signup" className="link">
+                        Sign-Up
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
