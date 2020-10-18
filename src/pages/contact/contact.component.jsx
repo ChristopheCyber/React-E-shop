fe03a1e725ae376aae824aeb3a9c6f231b4c5aa0
Christophe <christophalpha@yahoo.com>
2020-10-18 18:15:18 +0200

    S154; better design & contact logo and link to LinkedIn profile

--- src/pages/contact/contact.component.jsx
+++ src/pages/contact/contact.component.jsx
@@ -7,7 +7,29 @@ const ContactComponent = () => {
         <h5>Front End Developer :</h5>
             <h1>Schlienger Christophe</h1>
             <br/>
-            France / Czech Republic
+            <h1>( France / Czech Republic )</h1>
+            <br/>
+            <br/>
+            <div>
+{/*
+                 <img src="../../assets/logo-LinkedIn.jpg" alt="LinkedIn" />
+                 <a href="https://www.linkedin.com/in/christophe-schlienger-04164a1a8/">
+                     LinkedIn profile
+                 </a>
+  */}
+                <a  className="link-logo" 
+                    href="https://www.linkedin.com/in/christophe-schlienger-04164a1a8/"
+                    target="_blank" rel="noopener noreferrer">
+                    LinkedIn profile:
+                    <img 
+                        // src={require(`../../assets/logo-LinkedIn.jpg`)}
+                        src={require(`../../assets/logo-banner-LinkedIn.png`)}
+                        alt="LinkedIn profile"
+                        // title="LinkedIn profile"
+                        className="link-logo-img" 
+                    />
+                </a>
+                </div>
         </div>
     );
 }
