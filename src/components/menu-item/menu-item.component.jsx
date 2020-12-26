10ac5b7015d4cb7ceaa7475e9020a0123e350217
Christophe <christophalpha@yahoo.com>
2020-12-26 16:01:06 +0100

    add carousel on homepage

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,43 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './menu-item.styles.scss';
 
 import { withRouter } from 'react-router-dom';
+// for carousel :
+import CarouselComponent from "../../components/reusable-components/carousel/carousel.component.jsx";
 
-const MenuItem = (item) => {
-    return (
-        <div className={`menu-item ${item.size}`} >
-            <div className={`background-image ${item.imageBackOn}`}
-                style={{ //dynamic CSS
-                    // <img src={ require(`./img/img${prop1.cardProp2.id}.jpg`)} 
-                    // alt={`Pic ${prop1.cardProp2.id}`} 
-                    // title={`Image ${prop1.cardProp2.id}`}
-                    backgroundImage: `url(${require(`./img/${item.imageBack}`)})`
-                }}
-            >
-                <img className="image"
-                    src={require(`./img/${item.image}`)}
-                    alt={`Pic ${item.image}`} />
-            </div>
-            <div className="content">
-                <h1 className="title">
-                    {/*item.title.toLowerCase()*/}
-                    {item.title}
-                </h1>
-                <span className="subtitle link"
-                    onClick={() => (
-                        item.history.push(`${item.match.url}${item.title.toLowerCase()}`)
-                    )}
+class MenuItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showPopUp: false
+        };
+    }
+    togglePop = () => {
+        this.setState({
+            showPopUp: !this.state.showPopUp
+        });
+    };
+    render() {
+        return (
+            <div className={`menu-item ${this.props.size} 
+                ${this.state.showPopUp ? 'showPopUp' : ''}` } >
+                <div className={`background-image ${this.props.imageBackOn}`}
+                    style={{ //dynamic CSS
+                        // <img src={ require(`./img/img${prop1.cardProp2.id}.jpg`)} 
+                        // alt={`Pic ${prop1.cardProp2.id}`} 
+                        // title={`Image ${prop1.cardProp2.id}`}
+                        backgroundImage: `url(${require(`./img/${this.props.imageBack}`)})`
+                    }} onClick={this.togglePop}
                 >
-                    {item.linkUrl}
-                    {/*if using Link instead of HOC withRouter :
-                    <Link to="/paintings">{item.linkUrl}</Link>
-                    <Link to={`./${item.title.toLowerCase()}`} className="link">
-                        {item.linkUrl}
+                    <img className="image"
+                        src={require(`./img/${this.props.image}`)}
+                        alt={`Pic ${this.props.image}`} />
+                </div>
+                {this.state.showPopUp ? <CarouselComponent className="CarouselComponent" toggle={this.togglePop} /> : null}
+                <div className="content">
+                    <h1 className="title">
+                        {/*this.props.title.toLowerCase()*/}
+                        {this.props.title}
+                    </h1>
+                    <span className="subtitle link"
+                        onClick={() => (
+                            this.props.history.push(`${this.props.match.url}${this.props.title.toLowerCase()}`)
+                        )}
+                    >
+                        {this.props.linkUrl}
+                        {/*if using Link instead of HOC withRouter :
+                    <Link to="/paintings">{this.props.linkUrl}</Link>
+                    <Link to={`./${this.props.title.toLowerCase()}`} className="link">
+                        {this.props.linkUrl}
                     </Link>*/}
-                </span>
+                    </span>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default withRouter(MenuItem);
---
+++ src/components/reusable-components/carousel/carousel.component.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import './carousel.styles.scss';
+
+class CarouselComponent extends Component {
+    handleClick = () => {
+        this.props.toggle();
+       };
+    render() {
+        return (
+            <div className="carousel-wrapper carousel-wrapper-local">
+            <span className="close" onClick={this.handleClick}>&times; Close</span>
+            <Carousel infiniteLoop useKeyboardArrows autoPlay showThumbs axis={"horizontal"} onClickItem={this.handleClick}>
+                <div>
+                    <img src={require(`../../menu-item/img/FashionBack.jpg`)} alt="" />
+                </div>
+                <div>
+                    <img src={require(`../../menu-item/img/PaintingsBack.jpg`)} alt="" />
+                </div>
+                <div>
+                    <img src={require(`../../menu-item/img/MenBack.jpg`)} alt="" />
+                </div>
+                <div>
+                    <img src={require(`../../menu-item/img/WomenBack.jpg`)} alt="" />
+                </div>
+            </Carousel>
+        </div>
+        );
+    }
+}
+
+export default CarouselComponent;
